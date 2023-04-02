b82142a90bdef076953f3feb2385f870f9e56a1e
Pedro Mattos <laranjeiraspedro@gmail.com>
2023-04-02 13:07:23 -0300

    add: date picker

---
+++ src/components/DatePicker/DatePickerComponent.vue
@@ -0,0 +1,32 @@
+<template>
+  <date-picker :value="datePickerValue" @change="changeValue"></date-picker>
+</template>
+
+<script>
+import DatePicker from "vue-datepicker-next";
+import "vue-datepicker-next/index.css";
+export default {
+  data() {
+    return {
+      datePickerValue: null,
+    };
+  },
+  components: {
+    DatePicker,
+  },
+  methods: {
+    changeValue(dateValue) {
+      this.datePickerValue = dateValue;
+      this.$emit("dateTerm", dateValue);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+::v-deep {
+  input[name="date"] {
+    height: 40px;
+  }
+}
+</style>
