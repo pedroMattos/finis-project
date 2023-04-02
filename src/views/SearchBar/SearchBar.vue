e72c99b68b8243566a7391450372d9d8f40d2bf1
Pedro Mattos <laranjeiraspedro@gmail.com>
2023-04-02 13:08:13 -0300

    add date picket and search input on search bar

--- src/views/SearchBar/SearchBar.vue
+++ src/views/SearchBar/SearchBar.vue
@@ -1,16 +1,36 @@
 <template>
   <nav>
-    <p>Barra de busca</p>
+    <SearchInput @searchTerm="search" label="Buscar" />
+    <Datepicker @dateTerm="searchDate" />
   </nav>
 </template>
+
+<script>
+import Datepicker from "@/components/DatePicker/DatePickerComponent.vue";
+import SearchInput from "@/components/SearchInput/SearchInput.vue";
+
+export default {
+  components: { SearchInput, Datepicker },
+  methods: {
+    search(value) {
+      console.log(value);
+    },
+    searchDate(value) {
+      console.log(value);
+    },
+  },
+};
+</script>
 
 <style lang="scss" scoped>
 nav {
   width: 60%;
-  height: 40px;
-  background-color: blue;
+  max-height: fit-content;
+  background-color: #dedede;
   margin-bottom: 5px;
   border-radius: 5px;
-  margin: 0 auto 5px;
+  margin: 12px auto 5px;
+  padding: 8px;
+  display: flex;
 }
 </style>
